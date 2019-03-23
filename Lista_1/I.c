aa854561cb2238c788ce022bb70fd5e80ac12b3d
Ian Rocha <ianrocha.df@gmail.com>
2019-03-23 14:01:17 -0300

    Resolução da questão i

--- Lista_1/I.c
+++ Lista_1/I.c
@@ -4,8 +4,8 @@
 int main(){
     int cont = 0;
     char string[102];
-    while(scanf("%[^\n]%*c", string) != EOF){
-        for(int i = 0; i < 1005; i++){
+    while(fgets(string, 102, stdin) != NULL){
+        for(int i = 0; i < strlen(string); i++){
             if(string[i] == 111){
                 cont++;
             }

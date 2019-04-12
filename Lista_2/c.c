5f194863331e8b905be1a4cacf9cf60a85405630
Ian Rocha <ianrocha.df@gmail.com>
2019-04-12 20:12:28 -0300

    Finalização da Lista 2

--- Lista_2/c.c
+++ Lista_2/c.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
-int grau9(long long int n);
+int soma(char *string, int grau);
 
 int main(){
-    unsigned long long int n, resul, grau;
-    scanf("%lld", &n);
-    printf("%lld\n", n);
-    while(n != 0){
-        if(n % 9 == 0){
-            grau = grau9(n);
-            printf("%lld is a multiple of 9 and has 9-degree %lld.\n", n, grau);
+    char num[1000];
+    scanf("%s", num);
+
+    while(num[0] != '0'){
+        int grau = soma(num, 1);
+
+        if(grau == -1){
+            printf("%s is not a multiple of 9.\n", num);
         }
         else{
-            printf("%lld is not a multiple of 9.\n", n);
+            printf("%s is a multiple of 9 and has 9-degree %d\n", num, grau);
         }
 
-        scanf("%lld", &n);
+        scanf("%s", num);
     }
 return 0;
 }
 
-int grau9(long long int n){
-    int grau = 0;
-    if(n == 9){
-        return 1;
+int soma(char *string, int grau){
+    int soma = 0;
+    int tamanho = strlen(string);
+    for(int i = 0; i < tamanho; i++){
+        soma += string[i] - 48;
     }
-    else{
-        grau += grau9(n / 10);
-        return grau;
+
+    if(soma < 9){
+        return -1;
     }
+
+    if(soma > 9){
+        char aux[1000];
+        sprintf(aux, "%d", soma);
+        return soma*(aux, grau+1);
+    }
+    return grau;
 }

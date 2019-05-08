00ddf8dec46b7b8c40f09be4ba784d68fcc7ad38
Ian Rocha <ianrocha.df@gmail.com>
2019-05-08 10:43:09 -0300

    Primeira tentativa da 1 questão

--- Lista_4/a.c
+++ Lista_4/a.c
@@ -6,20 +6,18 @@ int main(){
     scanf("%d", &n);
 
     int v[n];
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){ // Preenchendo o vetor de notas
         scanf("%d", &v[i]);
     }
 
-    int aux[n];
-    for(int i = 0; i < n; i++){
-        int repet = 0;
-        for(int j = 0; j < n; j++){
-            if(j == i){
-                aux[i] = v[i];
-            }
-            if(aux[i] == v[n]);
-            
-        }
+    int aux[202];
+    for(int i = 0; i < 202; i++){
+        aux[i] = 0;
     }
+
+    for(int i = 0; i < n; i++){
+        
+    }
+
 return 0;
 }

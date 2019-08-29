3104a9c327b1c3039d7fe37bec5b97ada6ba213c
Ian Rocha <ianrocha.df@gmail.com>
2019-08-29 12:34:50 -0300

    Terminando a primeira questão da Lista 4

--- Lista_4/a.c
+++ Lista_4/a.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 
 int main(){
-    int n;
-    scanf("%d", &n);
+int n, nota[200],mnota,mfrequencia;
+mnota=mfrequencia=0;
+scanf("%d",&n);
+for (int i = 0; i < n; i++) {
+  scanf("%d",&nota[i]);
+}
 
-    int v[n];
-    for(int i = 0; i < n; i++){ // Preenchendo o vetor de notas
-        scanf("%d", &v[i]);
-    }
-
-    int aux[202];
-    for(int i = 0; i < 202; i++){
-        aux[i] = 0;
-    }
-
-    for(int i = 0; i < n; i++){
-        
-    }
-
-return 0;
+for (int i = 1,f; i < n; i++) {
+  f=0;
+  while(nota[i-1] == nota[i] && i < n)
+        {
+            f++;
+            i++;
+}
+        if(mfrequencia <= f && f > 0)
+        {
+            mfrequencia = f;
+            mnota = nota[i-1];
+        }
+        else if(mfrequencia == 0)
+        {
+            mnota = nota[i];
+        }
+}
+printf("%d\n", mnota);
+    return 0;
 }
